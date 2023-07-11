17394892a97be862f52529f5d032320c7f387302
INOPIAE <m.maengel@inopiae.de>
2023-07-11 15:45:26 +0200

    add: move to table output

--- src/visual.ts
+++ src/visual.ts
@@ -8,7 +8,24 @@ import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructor
 import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
 import IVisual = powerbi.extensibility.visual.IVisual;
 
+import DataView = powerbi.DataView;
+import DataViewObjects = powerbi.DataViewObjects;
+import DataViewValueColumn = powerbi.DataViewValueColumn;
+import DataViewTable = powerbi.DataViewTable;
+import DataViewTableRow = powerbi.DataViewTableRow;
+
+import DataViewMetadataColumn = powerbi.DataViewMetadataColumn;
+import DataViewCategoryColumn = powerbi.DataViewCategoryColumn;
+import PrimitiveValue = powerbi.PrimitiveValue;
+import IVisualHost = powerbi.extensibility.visual.IVisualHost;
+
+import { valueFormatter as vf, textMeasurementService as tms } from "powerbi-visuals-utils-formattingutils";
+import IValueFormatter = vf.IValueFormatter;
+
+
 import { VisualFormattingSettingsModel } from "./settings";
+
+import * as $ from "jquery";
 
 export class Visual implements IVisual {
     private target: HTMLElement;
@@ -16,29 +33,123 @@ export class Visual implements IVisual {
     private textNode: Text;
     private formattingSettings: VisualFormattingSettingsModel;
     private formattingSettingsService: FormattingSettingsService;
+    private rootElement: JQuery;
+    private dataView: DataView;
+    private hostService: IVisualHost;
 
     constructor(options: VisualConstructorOptions) {
         console.log('Visual constructor', options);
         this.formattingSettingsService = new FormattingSettingsService();
-        this.target = options.element;
-        this.updateCount = 0;
-        if (document) {
-            const new_p: HTMLElement = document.createElement("p");
-            new_p.appendChild(document.createTextNode("Update count:"));
-            const new_em: HTMLElement = document.createElement("em");
-            this.textNode = document.createTextNode(this.updateCount.toString());
-            new_em.appendChild(this.textNode);
-            new_p.appendChild(new_em);
-            this.target.appendChild(new_p);
-        }
+
+        this.hostService = options.host;
+        this.rootElement = $(options.element);
+
     }
 
     public update(options: VisualUpdateOptions) {
         this.formattingSettings = this.formattingSettingsService.populateFormattingSettingsModel(VisualFormattingSettingsModel, options.dataViews);
 
         console.log('Visual update', options);
-        if (this.textNode) {
-            this.textNode.textContent = (this.updateCount++).toString();
+
+        this.rootElement.empty();
+
+        var dataView: DataView = this.dataView = options.dataViews[0];
+
+        if (dataView != null && dataView.table != null) {
+
+            var table: DataViewTable = dataView.table;
+            var columns: DataViewMetadataColumn[] = table.columns;
+            var rows: DataViewTableRow[] = table.rows;
+
+            var dsgvoTable: JQuery = $("<table>", { id: "dsgvoTable" });
+            var headerRow: JQuery = $("<tr>");
+            for (var headerIndex: number = 0; headerIndex < columns.length; headerIndex++) {
+                headerRow.append($("<th>").text(columns[headerIndex].displayName));
+            }
+            dsgvoTable.append(headerRow);
+
+            var mainsubject = null;
+            var strOther = "Other";//this.formattingSettings.dsgvo.otherName.value;
+            var intOther = 0;
+            var intOtherMargin = 5;//this.formattingSettings.dsgvo.otherLimit.value;
+            var intSubtotal = 0;
+            var intTotal = 0;
+
+            var blnHideMainsubject: boolean = false;
+            var blnShowSubtotal = false;
+            var blnShowTotal = false;
+
+            for (var rowIndex: number = 0; rowIndex < rows.length; rowIndex++) {
+
+                if (mainsubject === null) {
+                    mainsubject = rows[rowIndex][0].valueOf();
+                }
+                else if (mainsubject != rows[rowIndex][0].valueOf()) {
+                    // handle main subject
+                    var topic = mainsubject;
+                    if (blnHideMainsubject != false) {
+                        topic = "";
+                    }
+                    this.addRow(dsgvoTable, topic, strOther, intOther, columns);
+
+                    // handle sub total
+                    if (blnShowSubtotal != false) {
+                        topic = "Subtotal of " + mainsubject;
+                        this.addRow(dsgvoTable, topic, "", intSubtotal, columns, true);
+                        intSubtotal = 0;
+                    }
+
+                    mainsubject = rows[rowIndex][0].valueOf();
+                }
+
+                
+
+                var firstColumn: PrimitiveValue = rows[rowIndex][0].valueOf();
+                var secondColumn: PrimitiveValue = rows[rowIndex][1].valueOf();
+                var thirdColumn: PrimitiveValue = rows[rowIndex][2].valueOf();
+                if (intOtherMargin < Number(rows[rowIndex][2].valueOf())) {
+                    this.addRow(dsgvoTable, firstColumn, secondColumn, thirdColumn, columns, true);
+                } else{
+                    intOther += Number(rows[rowIndex][2].valueOf());
+                }
+
+                intSubtotal += Number(rows[rowIndex][2].valueOf())
+                intTotal += Number(rows[rowIndex][2].valueOf())
+            }
+
+            // handle final rows
+            var topic = mainsubject;
+            if (blnHideMainsubject != false) {
+                topic = "";
+            }
+            this.addRow(dsgvoTable, topic, strOther, intOther, columns);
+
+            // handle sub total
+            if (blnShowSubtotal != false) {
+                topic = "Subtotal of " + mainsubject;
+                this.addRow(dsgvoTable, topic, "", intSubtotal, columns, true);
+                intSubtotal = 0;
+            }
+            if (blnShowTotal != false) {
+                topic = "Grand total";
+                this.addRow(dsgvoTable, topic, "", intTotal, columns, true);
+                intSubtotal = 0;
+            }
+
+            var dsgvoTableContainer: JQuery = $("<div>", { id: "dsgvoTableContainer" });
+            dsgvoTableContainer.css({
+                "width": options.viewport.width,
+                "height": options.viewport.height
+            });
+
+                var dsgvoTableContainer: JQuery = $("<div>", { id: "dsgvoTableContainer" });
+                dsgvoTableContainer.css({
+                    "width": options.viewport.width,
+                    "height": options.viewport.height
+                });
+                dsgvoTableContainer.append(dsgvoTable);
+                this.rootElement.append(dsgvoTableContainer);
+            
         }
     }
 
@@ -48,5 +159,36 @@ export class Visual implements IVisual {
      */
     public getFormattingModel(): powerbi.visuals.FormattingModel {
         return this.formattingSettingsService.buildFormattingModel(this.formattingSettings);
+    }
+
+    private addRow(dsgvoTable, firstColumn: any, secondColumn: any, thirdColumn: any, columns: any, bold: boolean = false): void {
+        var tableRow: JQuery = $("<tr>");
+
+        var tableCell: JQuery = $("<td>").text(firstColumn);
+        // if (this.getValue<boolean>(columns[0].objects, "columnFormatting", "fontBold", false) || bold === true) {
+        //     tableCellOther.css({ "font-weight": "bold" });
+        // }
+        tableRow.append(tableCell);
+        tableCell = $("<td>").text(secondColumn);
+        // if (this.getValue<boolean>(columns[1].objects, "columnFormatting", "fontBold", false) || bold === true) {
+        //     tableCellOther.css({ "font-weight": "bold" });
+        // }
+        tableRow.append(tableCell);
+
+        var valueFormat: string = columns[2].format;
+
+        var valueFormatter = vf.create({
+            format: valueFormat,
+            cultureSelector: this.hostService.locale
+        });
+
+        tableCell = $("<td>").text(valueFormatter.format(thirdColumn));
+        // if (this.getValue<boolean>(columns[2].objects, "columnFormatting", "fontBold", false) || bold === true) {
+        //     tableCellOther.css({ "font-weight": "bold" });
+        // }
+        tableCell.css({ "text-align": "right" });
+
+        tableRow.append(tableCell);
+        dsgvoTable.append(tableRow);
     }
 }
